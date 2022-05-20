c385124f2cc87a36b412b968071961009371e113
jh <jh@jhui-MacBookPro-2.local>
2022-05-20 15:03:19 +0900

    개발환경세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { styled } from "style/theme";
+import Buttons from "components/buttons/Buttons";
+import { globalStyles } from "style/globalStyles";
 
 function App() {
+  globalStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test>123</Test>
+      <Buttons />
     </div>
   );
 }
 
 export default App;
+
+const Test = styled("div", { fontSize: "$3" });
